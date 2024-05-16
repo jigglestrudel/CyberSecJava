261e4a08e1076f419f2ebb123aae5ac9b6a1c7bb
agatherat <kulesz.agnieszka@wp.pl>
2024-05-16 19:01:07 +0200

    początki są trudne, ale miło jest skońćzyć :) RSA i DSA działąją, EC nie jest skory do współpracy #notslaying

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        SignatureGenerator generatorPodpisow = new SignatureGenerator("C:\\Users\\kules\\Desktop\\studia\\aga.txt", "EC");
+        generatorPodpisow.generateSignature();
+        System.out.println("DONE");
+
     }
 }
